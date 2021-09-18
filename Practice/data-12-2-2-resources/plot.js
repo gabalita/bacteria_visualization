c1cc8764b9208ab6fb1c8a5497073f3cdc0ee737
gabalita <gabalita@gmail.com>
2021-09-18 18:53:00 +0100

    changing html for deliberable 1

--- Practice/data-12-2-2-resources/plot.js
+++ Practice/data-12-2-2-resources/plot.js
@@ -1,14 +1,21 @@
+// Save array of cities sorted highest to lowest
 var sortedCities = cityGrowths.sort((a,b)=> 
 a.Increase_from_2016 - b.Increase_from_2016).reverse();
-console.log(sortedCities)
+console.log("List of sorted cities: ", sortedCities)
 
+// Save a new array of top 5 cities
 var topFiveCities = sortedCities.slice(0,5);
-console.log(topFiveCities);
+console.log("Top 5 cities are: ", topFiveCities);
 
-// Make arrays of city names and city populations
+// Grab the names and populations of arrays
+// For example, topFiveCities is an array of objects ... [{}, {}, {}...]
+// For each element (object) in the array, we are writing a function that takes each object and returns the city (city.City) 
+// and puts them in a new array.
 var topFiveCityNames = topFiveCities.map(city => city.City);
 var topFiveCityPopulations = topFiveCities.map(city => parseInt(city.Increase_from_2016));
 
+console.log("Names of cities are: ", topFiveCityNames);
+console.log("Populations of cities are: ", topFiveCityPopulations);
 
 // Create a bar chart
 var trace = [{
@@ -23,4 +30,5 @@ var layout = {
     yaxis: { title: "Population Growth, 2016-2017"}
   };
 
-  Plotly.newPlot("bar-plot", trace, layout);
+// Plot the graph by referencing the id in our index.html file
+Plotly.newPlot("bar-plot", trace, layout);
--- Practice/data-12-3-2-resources/plots.js
+++ Practice/data-12-3-2-resources/plots.js
@@ -1,3 +1,19 @@
+// Overview of Concepts:
+// d3.json(url) method will allow us to read external JSON files, as well as place calls to external web APIs for data
+//     - a JavaScript promise in this case waits for the data retrieval to finish before moving on to the next code.
+// We are dynamically creating dropdown menu links by:
+//    1) saving dropdown menu as selector variable 
+//    2) We append our options to selector through forEach element in the object, names
+//    3) <select id="selDataset" onchange="optionChanged(this.value)"></select>
+//      a) select tag now has attribute "onchange", which is associated with optionChanged function. 
+//      b) anytime a change takes place, optionChanged function is called where the argument is this.value (which 
+//        is assigned the current value of the dropdown menu; in the context of an event, it refers to the HTML element that received the event)
+
+//  function optionChanged(newSample) {
+  // buildMetadata(newSample);
+  // buildCharts(newSample);}
+//    The argument, newSample, is the volunteer ID number that is passed to both of these functions
+
 // Define the initialize function
 function init() {
   var selector = d3.select("#selDataset");
